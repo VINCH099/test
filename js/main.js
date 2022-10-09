bf73220eee78af42e14947db62c57cebc7591a44
VINCH <109498440+VINCH099@users.noreply.github.com>
2022-10-09 04:39:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,10 +20,18 @@ menu.querySelectorAll('.menu__link').forEach((link) => {
   })
 })
 
-logo.querySelectorAll('.header__logo').forEach((link) => {
-  link.addEventListener('click', () => {
-    menu.classList.remove('active')
-    burger.classList.remove('active')
-    body.classList.remove('lock')
-  })
-})
+// logo.querySelectorAll('.logo').forEach((link) => {
+//   link.addEventListener('click', () => {
+//     menu.classList.remove('active')
+//     burger.classList.remove('active')
+//     body.classList.remove('lock')
+//   })
+// })
+
+// inputmask
+
+const form = document.querySelector('.sale__form')
+const telSelector = form.querySelector('input[type="tel"]')
+const inputMask = new Inputmask('+38 (999) 999-99-99')
+
+inputMask.mask(telSelector)
